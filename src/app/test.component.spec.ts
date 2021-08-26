f44aab111c7026b52bd15de8098378ed01e303c8
Maksym Kobieliev <maksym.kobieliev@gmail.com>
2021-08-26 11:09:54 +0300

    Reproducible without graphql/apollo

--- src/app/test.component.spec.ts
+++ src/app/test.component.spec.ts
@@ -1,6 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 
-import { TestComponent } from './test.component';
+import * as Exported from './exported';
+
+@Component({
+  selector: 'app-test',
+  template: 'template',
+})
+export class TestComponent {
+  constructor(private exported: Exported.Exported) {}
+}
 
 describe('TestComponent', () => {
   beforeEach(async () => {
